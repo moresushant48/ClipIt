08a15f18e4a8b0055498cb013caa4a3f287960d6
Sushant More <moresushant48@gmail.com>
2021-11-28 16:09:57 +0530

    Setup firebase
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class Constants {
   static const pageLimit = 10;
-  static const baseUrl = "http://192.168.1.100:3020";
+  static const baseUrl = "http://192.168.1.101:3020";
   static BaseOptions networkOptions = BaseOptions(
     baseUrl: baseUrl,
     connectTimeout: 150000,
